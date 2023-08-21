aebadfefc795209388cec44584e89e7a636c4277
Somenath Choudhury <somenathchoudhury35@gmail.com>
2023-08-21 11:20:53 +0530

    commited

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = ({ children }) => {
     <div>
 
         <nav className="flex items-center justify-between p-4">
-            <p className="text-xl lg:text-2xl ml-8 text-violet-700 font-semibold tracking-wide">SMS Spam Classifier</p>
+            <p className="text-xl lg:text-2xl ml-8 text-violet-700 font-semibold tracking-wide">Spam SMS Classifier</p>
         </nav>
 
         {children}
